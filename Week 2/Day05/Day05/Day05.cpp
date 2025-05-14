cdceef4a16e304ae1b9028b01be73b6aa45113d8
Garrett Girod <girodg@hotmail.com>
2025-05-14 12:07:49 -0400

    day 5: searching, maps

--- Week 2/Day05/Day05/Day05.cpp
+++ Week 2/Day05/Day05/Day05.cpp
@@ -7,12 +7,26 @@
 #include <vector>
 #include "Console.h"
 #include "Input.h"
+#include <iomanip>
 
 enum class Weapon
 {
     Sword, Axe, Spear, Mace
 };
 
+int LinearSearch(const std::vector<int>& searchVector, int searchNumber)
+{
+    int foundIndex = -1;//NOT FOUND
+    for (int i = 0; i < searchVector.size(); i++)
+    {
+        if (searchNumber == searchVector[i])
+        {
+            foundIndex = i;
+            break;
+        }
+    }
+    return foundIndex;
+}
 
 int main()
 {
@@ -36,8 +50,15 @@ int main()
                 4) if reach the end of the vector, return -1 which means not found
 
     */
+    //N = size, O(N)
     std::vector<int> numbers = { 0,1,2,3,4,5,6 };
-    int searchNumber = 15;
+    int searchNumber = 6;
+    int index = LinearSearch(numbers, searchNumber);
+    if (index == -1)
+        std::cout << searchNumber << " not found.\n";
+    else
+        std::cout << searchNumber << " found at index " << index << "\n";
+
 
 
 
@@ -77,18 +98,119 @@ int main()
     dorasBackpack[Weapon::Axe] = 7;//simply overwrites the value if the key is already in the map
 
 
+
+    // "eggs" 4.99
+    std::map<std::string,float> menu;
+    //To add to a map...
+    //1) map[key] = value;
+    menu["coffee"] = 1.99f;
+    menu["sausage mcmuffin"] = 4.99f;
+    menu["mcGriddle"] = 5.99f;
+    menu["mcGriddle"] = 7.99f;//overwrites the old value
+
+    //2) map.insert(pairObject)
+    //stores data as a key-value pair object
+    auto pairToInsert = std::make_pair<std::string, float>("mcGriddle", 3.99f);
+
+    std::pair<std::map<std::string,float>::iterator,bool> insertedItem = 
+        menu.insert(pairToInsert); //does NOT overwrite an existing value
+    //first is the ITERATOR to the item in the map
+
+    //pairs have 2 parts: first, second
+    if (insertedItem.second) //pair was inserted
+    {
+        std::cout << pairToInsert.first << " was inserted.\n";
+    }
+    else
+    {
+        std::cout << pairToInsert.first << " was already on the menu.\n";
+    }
+
+    //range-based loop
+    std::cout << "\n\nrange-based loop\n";
+    for (auto& item : menu)
+    {
+        //first is the key (item name)
+        //second is the value (item price)
+        std::cout << item.first << " " << item.second << "\n";
+    }
+    std::cout << "\n\nmodern range-based loop\n";
+    for (const auto& [itemName,itemPrice] : menu)
+    {
+        //itemPrice += 5;
+        std::cout << itemName << " " << itemPrice << "\n";
+    }
+
+    std::cout << "\n\nIterator for-loop\n";
+    for (auto iter = menu.begin(); iter != menu.end(); iter++)
+    {
+        std::cout << iter->first << " " << (*iter).second << "\n";
+    }
+
+    std::cout << "\n\nIterator while-loop\n";
+    auto whileIter = menu.begin();
+    while (whileIter != menu.end())
+    {
+        std::cout << whileIter->first << " " << (*whileIter).second << "\n";
+        whileIter++;
+    }
+
+
+    //find coffee on the menu
+    //find(key). returns an iterator.
+    std::string itemToFind = "coffee";
+    auto foundMenuItem = menu.find(itemToFind);
+    if (foundMenuItem != menu.end())
+    {
+        //menu[foundMenuItem->first] = foundMenuItem->second * 1.10;
+        float oldPrice = foundMenuItem->second;
+        foundMenuItem->second *= 1.10;
+        std::cout << "Coffee used to costs " << oldPrice << "\n";
+        std::cout << "Coffee now costs " << foundMenuItem->second << "! Thanks PUTIN!!\n";
+    }
+    else
+        std::cout << "Coffee is not on the menu. WHAAAAT?!\n";
+
     /*
         CHALLENGE:
 
-            Create a map that stores names (string) and grades. Call the variable grades.
+            Create a map that stores names (string) and grades. 
+            Call the variable grades.
             Add students and grades to your map.
 
     */
+    std::map<std::string, double> grades;
 
+    std::vector<std::string> students{
+        "Garrett", "Emrys", "Vincent", "Jakob", "Vintey", "Aaden", "Corey", "June",
+        "Aaron", "Asher", "Edwin", "Jacob", "James", "Jesse", "Mathurin", "Melissa",
+        "Mykell", "Tom", "Wiley"
+    };
+    srand(time(NULL));
+    for (auto& student : students)
+    {
+        grades[student] = rand() % 10001 / 100.0;
+    }
 
+    /*
+        CHALLENGE:
 
+            Loop over your grades map and print each student name and grade.
 
-
+    */
+    std::cout << "\n\nPG2 2505\n";
+    for (auto& [student,grade] : grades)
+    {
+        Console::SetForegroundColor(
+            (grade < 59.5) ? ConsoleColor::Red :
+            (grade < 69.5) ? ConsoleColor::Yellow :
+            (grade < 79.5) ? ConsoleColor::Magenta :
+            (grade < 89.5) ? ConsoleColor::Blue :
+            ConsoleColor::Green);
+        std::cout << std::right << std::setw(8) << grade;
+        Console::Reset();
+        std::cout << std::left << "  " << student << "\n";
+    }
 
     /*
         ╔═══════════════════╗
@@ -121,14 +243,6 @@ int main()
         }
         std::cout << val << "\n";
     }
-
-
-    /*
-        CHALLENGE:
-
-            Loop over your grades map and print each student name and grade.
-
-    */
 
 
 
